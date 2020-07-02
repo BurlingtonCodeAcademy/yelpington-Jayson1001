af02c9209a1377c4506ec1c1105853372af6e4d5
Jayson1001 <64107977+Jayson1001@users.noreply.github.com>
2020-07-02 15:07:09 -0400

    Thurs3pm

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 const express = require('express')
+const fetch = require('fetch')
+const path = require('path')
+const port = process.env.PORT || 5050
+const app = express()
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.send(path.resolve('public/index.html'))
+});
+
+
+app.listen(port, () => console.log(`Listening on port: ${port}`))
