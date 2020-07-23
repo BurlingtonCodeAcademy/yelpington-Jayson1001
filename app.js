4c583c0f37c1ed628d86f73dd62a0920e36574a6
Jayson1001 <64107977+Jayson1001@users.noreply.github.com>
2020-07-23 14:52:59 -0400

    REFACTORED-SUBMISSION

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ const port = process.env.PORT || 5050
 const app = express()
 app.use(express.static('public'));
 
+//-----path to my index.html home page-----//
 app.get('/', (req, res) => {
     res.send(path.resolve('/public/index.html'))
 });
 
+//-----Path to my restaurant.html page after clicking specific restaurant on home page-----//
 app.get('/restaurant', (req, res) => {
     res.sendFile(path.resolve('public/restaurant.html'))
 });
